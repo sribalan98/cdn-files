09431e929b8a8fdf2d8ca9fa9da9c7ea39da8e60
sribalan98 <105964659+sribalan98@users.noreply.github.com>
2025-12-12 23:46:41 +0530

    Add Sparticle configuration with multiple image URLs

--- data.js
+++ data.js
@@ -1,3 +1,34 @@
 window.coderender = {
   tickerText: "ORDER ORDER NOW !!",
+  Sparticle: {
+    composition: "source-over",
+    count: 48,
+    speed: 8.5,
+    parallax: 1.7,
+    direction: 140,
+    xVariance: 0,
+    yVariance: 2,
+    rotate: true,
+    rotation: 1,
+    alphaSpeed: 10,
+    alphaVariance: 5,
+    minAlpha: 2,
+    maxAlpha: 2,
+    minSize: 6,
+    maxSize: 20,
+    style: "fill",
+    bounce: false,
+    drift: 0.4,
+    glow: 50,
+    twinkle: false,
+    color: ["#ffffff"],
+    shape: "image",
+
+    // ⭐ MULTIPLE IMAGES HERE
+    imageUrl: [
+      "https://image2url.com/images/1765523059468-bcd9a8d5-4daa-4747-b2fd-2185d72634ee.png",
+      "https://image2url.com/images/1765521804701-0cfb69c4-ee7d-441d-bb6f-2d2f563049ae.png",
+      "https://image2url.com/images/1765543839016-47cc52d7-edb9-4e07-8f9d-f6a1f580733f.png",
+    ],
+  },
 };
